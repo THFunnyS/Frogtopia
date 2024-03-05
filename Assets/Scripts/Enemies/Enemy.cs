899a756f3ecb1a704abdcb043a605da929385a39
JoYfanCode <semenmel592@gmail.com>
2024-03-06 03:03:37 +0400

    Fly can Shoot

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,6 +2,7 @@
 
 public abstract class Enemy : MonoBehaviour
 {
+    public int lives = 5;
     public float AgressDistance = 10f;
     public float Speed = 5f;
     public float RestTime = 2f;
@@ -17,6 +18,20 @@ public abstract class Enemy : MonoBehaviour
 
     public virtual void Update()
     {
-        SM.CurrentState.Update();
+        if(SM.CurrentState != null)
+            SM.CurrentState.Update();
+
+        if (lives == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "PlayerBullet")
+        {
+            lives--;
+        }
     }
 }
---
+++ Assets/Scripts/Enemies/Fly/Fly.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly : Enemy
+{
+    [SerializeField] private GameObject _bullet;
+
+    public override void Start()
+    {
+        base.Start();
+
+        StartCoroutine(CheckTarget());
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    IEnumerator CheckTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(3f);
+
+            if (Vector2.Distance(transform.position, Target.position) < AgressDistance)
+            {
+                GameObject bullet = Instantiate(_bullet, gameObject.transform);
+                Destroy(bullet, 5f);
+            }
+        }
+    }
+}
