0c3d9b104d15dd6d301ed9ac52c4c5337899f664
gagrdedi <gagrdedi@mail.ru>
2024-04-17 02:02:58 +0400

    Simple random generation

--- Assets/Scripts/ProceduralGenerationLevel1.cs
+++ Assets/Scripts/ProceduralGenerationLevel1.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class ProceduralGenerationLevel1 : MonoBehaviour
 {
+    private int roomSize = 40;
+    private string[] corridors = { "Rooms/Level1/corridor1", "Rooms/Level1/corridor2" };
+    private string[] underground = { "Rooms/Level1/underground1"};
+
     public GameObject prefab;
-    // Start is called before the first frame update
+    
     void Start()
     {
-        Instantiate(prefab, this.transform);
+        prefab = Resources.Load<GameObject>("Rooms/Level1/startRoom");
+        Instantiate(prefab, transform);
+        int idx = Random.Range(0, corridors.Length);
+        prefab = Resources.Load<GameObject>(corridors[idx]);
+        Instantiate(prefab, transform).transform.position += new Vector3(40, 0, 0);
+        
     }
 
     // Update is called once per frame
