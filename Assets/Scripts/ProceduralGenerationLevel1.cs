bc69a56713b123be2dd47f298982eee51568ed12
gagrdedi <gagrdedi@mail.ru>
2024-05-28 16:05:39 +0400

    Процедурная генерация в2
    Очередной proof of concept

--- Assets/Scripts/ProceduralGenerationLevel1.cs
+++ Assets/Scripts/ProceduralGenerationLevel1.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class ProceduralGenerationLevel1 : MonoBehaviour
 {
     private int roomSize = 40;
+    private string[] rooms = { "Rooms/Level1/2 (2)/Room_2_1", "Rooms/Level1/2 (2)/Room_2_2" };
     private string[] corridors = { "Rooms/Level1/corridor1", "Rooms/Level1/corridor2" };
     private string[] underground = { "Rooms/Level1/underground1"};
-
-    public GameObject prefab;
     
+    void placeRoom(string name, int x, int y)
+    {
+        GameObject prefab = Resources.Load<GameObject>(name);
+        Instantiate(prefab, transform).transform.position = new Vector3 (x * roomSize, y * roomSize, 0);
+    }
+    void placeRandomRoom(string[] rooms, int x, int y)
+    {
+        int idx = Random.Range(0, corridors.Length);
+        GameObject prefab = Resources.Load<GameObject>(rooms[idx]);
+        Instantiate(prefab, transform).transform.position = new Vector3(x * roomSize, y * roomSize, 0);
+    }
     void Start()
     {
-        prefab = Resources.Load<GameObject>("Rooms/Level1/startRoom");
-        Instantiate(prefab, transform);
-        int idx = Random.Range(0, corridors.Length);
-        prefab = Resources.Load<GameObject>(corridors[idx]);
-        Instantiate(prefab, transform).transform.position += new Vector3(40, 0, 0);
+        placeRoom("Rooms/Level1/2 (2)/AutoLayer", 0, 0);
+        placeRandomRoom(rooms, 1, 0);
         
     }
 
